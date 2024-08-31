e0a26d097b0ebc57e6336548daf8ebbffc4dd2f4
lovetruongan <lovetruongan@gmail.com>
2024-09-01 02:17:18 +0700

    fix Dockercompose

--- Training_FE/src/main.jsx
+++ Training_FE/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import CreatePatient from "./pages/CreatePatient";
 import SuccessPage from "./pages/SuccessPage";
 import EditPatient from "./pages/EditPatient";
